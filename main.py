a2b1c861fdcb56352d85e3dfea5c50f82a1c6d34
Vinícius Castro Coutinho <vcc.snf23@uea.edu.br>
2023-12-01 10:55:30 -0400

    colisão com o player

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame
-import os
 
 pygame.init()
 # window config
-height = 780
-width = 712
+HEIGHT = 780
+WIDTH = 712
 
 # Set the screen size and create a Pygame display
-size = (width, height)
+size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("BreakOut - PyGame Edition - 2023-11-30")
 
@@ -16,7 +15,7 @@ game_loop = True
 game_clock = pygame.time.Clock()
 
 # player
-player_x = width/2
+player_x = WIDTH/2
 player_y = 700
 player_move_left = False
 player_move_right = False
@@ -30,29 +29,24 @@ ball_y = 100
 
 def visuals():
     screen.fill((0, 0, 0))
-    global player
+    global player, left_wall, right_wall, ball
     player = pygame.draw.rect(screen, (000, 90, 137), (player_x, player_y, 70, 10))
 
     # walls
     upper_wall = pygame.draw.rect(screen, (255, 255, 255), (0, 0, 712, 60))
-    global left_wall
     left_wall = pygame.draw.rect(screen, (255, 255, 255), (0, 0, 10, 780))
-    global righ_wall
-    righ_wall = pygame.draw.rect(screen, (255, 255, 255), (702, 0, 10, 780))
+    right_wall = pygame.draw.rect(screen, (255, 255, 255), (702, 0, 10, 780))
 
     # colored walls
-    blue_left_wall = pygame.draw.rect(screen, (000, 90, 137), (0, 690, 10, 30))
-    blue_right_wall = pygame.draw.rect(screen, (000, 90, 137), (702, 690, 10, 30))
+    pygame.draw.rect(screen, (000, 90, 137), (0, 690, 10, 30))
+    pygame.draw.rect(screen, (000, 90, 137), (702, 690, 10, 30))
 
     # ball
-    global ball
     ball = pygame.draw.rect(screen, (255, 255, 0), (ball_x, ball_y, 10, 10))
 
 
 def commands(event):
-    global game_loop
-    global player_move_right
-    global player_move_left
+    global game_loop, player_move_right, player_move_left
 
     if event.type == pygame.QUIT:
         game_loop = False
@@ -69,9 +63,10 @@ def commands(event):
         if event.key == pygame.K_RIGHT:
             player_move_right = False
 
+
 def animations():
     # player pad animation
-    global player_x
+    global player_x, ball_y, ball_x
     if player_move_right and player_x < 630:
         player_x += 7
     else:
@@ -81,43 +76,34 @@ def animations():
         player_x -= 7
     else:
         player_x -= 0
-    #print(player,'player')
-    #print(ball)
 
     # ball animation
-    global ball_y
-    global ball_x
     ball_x += ball_speed_x
     ball_y += ball_speed_y
 
 
-
 def colliders():
-    global ball
-    global righ_wall
-    global ball_speed_x
-    global ball_speed_y
-    global ball_y
-    if ball.colliderect(righ_wall):
+    global ball, right_wall, ball_speed_x, ball_speed_y, ball_y
+    if ball.colliderect(right_wall):
         print('colidiu com direita')
     if ball.colliderect(left_wall):
         print('colidiu com esquerda')
-    if ball.colliderect(player):
+    if ball.colliderect(player) and ball_speed_y > 0:
         print('colidiu')
+        ball_y -= 10
         ball_speed_y *= -1
 
-
     # collision with wall
-
     if ball_x > 680:
         ball_speed_x *= -1
-    if ball_x < 10 :
+    if ball_x < 10:
         ball_speed_x *= -1
 
     if ball_y < 60:
         ball_speed_y *= -1
 
-while game_loop == True:
+
+while game_loop:
     # place the visuals
     visuals()
     # search for commands
@@ -127,7 +113,6 @@ while game_loop == True:
     # produce the animations
     animations()
     colliders()
-    print(ball_y)
     pygame.display.flip()
     game_clock.tick(60)
 
